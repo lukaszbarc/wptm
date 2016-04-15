f25980757a407f6a0136db3cfa66017997cb5d00
jbogacz <bogacz.jakub3@gmail.com>
2016-04-15 15:27:14 +0200

    Distance calculation between two nearest points fixed

--- components/client-service-impl/src/main/java/pl/ptm/client/service/impl/CachedVehicleStopServiceImpl.java
+++ components/client-service-impl/src/main/java/pl/ptm/client/service/impl/CachedVehicleStopServiceImpl.java
@@ -1,13 +1,11 @@
 package pl.ptm.client.service.impl;
 
-import com.grum.geocalc.DegreeCoordinate;
-import com.grum.geocalc.EarthCalc;
-import com.grum.geocalc.Point;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import pl.ptm.client.api.VehicleStopData;
 import pl.ptm.client.service.api.CachedVehicleStopService;
+import pl.ptm.common.service.api.DistanceCalculationService;
 import pl.ptm.data.dao.jpa.VehicleStopDaoJpa;
 import pl.ptm.data.model.VehicleStopEntity;
 
@@ -26,6 +24,9 @@ public class CachedVehicleStopServiceImpl implements CachedVehicleStopService {
     @Autowired
     private VehicleStopConverter converter;
 
+    @Autowired
+    private DistanceCalculationService distanceCalculationService;
+
     @Cacheable("vehiclesStops")
     @Override
     public List<VehicleStopData> getRegisteredVehicleStops() {
@@ -39,7 +40,6 @@ public class CachedVehicleStopServiceImpl implements CachedVehicleStopService {
     @Cacheable("nearestStop")
     @Override
     public VehicleStopData getNearestVehicleStop(Double lon, Double lat) {
-        Point stand = new Point(new DegreeCoordinate(lon), new DegreeCoordinate(lon));
 
         Double smallestDist = null;
         VehicleStopData nearestVehicleStop = null;
@@ -48,16 +48,35 @@ public class CachedVehicleStopServiceImpl implements CachedVehicleStopService {
             if(stopData.getLon() == null || stopData.getLat() == null){
                 continue;
             }
-
-            Point fore = new Point(new DegreeCoordinate(stopData.getLon()), new DegreeCoordinate(stopData.getLat()));
-
-            Double distance = EarthCalc.getDistance(stand, fore);
-            if(smallestDist == null || distance < smallestDist){
-                smallestDist = distance;
+            double currDistance = calculateDistanceInKm(lon, lat, stopData.getLon(), stopData.getLat());
+            if(smallestDist == null || currDistance < smallestDist){
+                smallestDist = currDistance;
                 nearestVehicleStop = stopData;
             }
         }
         return nearestVehicleStop;
+    }
+
+    private double calculateDistanceInKm(double lon1, double lat1, double lon2, double lat2) {
+        return distance(lon1, lat1, lon2, lat2);
+    }
+
+    private static double distance(double lat1, double lon1, double lat2, double lon2) {
+        double theta = lon1 - lon2;
+        double dist = Math.sin(deg2rad(lat1)) * Math.sin(deg2rad(lat2)) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.cos(deg2rad(theta));
+        dist = Math.acos(dist);
+        dist = rad2deg(dist);
+        dist = dist * 60 * 1.1515;
+        dist = dist * 1.609344;
+        return (dist);
+    }
+
+    private static double deg2rad(double deg) {
+        return (deg * Math.PI / 180.0);
+    }
+
+    private static double rad2deg(double rad) {
+        return (rad * 180 / Math.PI);
     }
 
 }
